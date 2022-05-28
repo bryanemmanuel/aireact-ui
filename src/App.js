93b49493275103fa614a267dea3bd8b925cb3347
bryanemmanuel <bryan.emmanuel.paz@gmail.com>
2022-05-28 14:47:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { Blog, Features, Footer, Header, Posibility, Whatgpt } from './containers';
+import { Article,Brand,CTA,Navbar } from './components';
 import './App.css';
 
-function App() {
+
+
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <Whatgpt />
+        <Features />
+        <Posibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
